01f65cfcc04f4156cf9463e36f5cfbab253eb2db
Hashmatullah Nazari <hn0153@gmail.com>
2024-10-13 09:25:29 -0500

    Create spring.js

---
+++ spring.js
@@ -0,0 +1,20 @@
+let themeName = 'Spring Festival';
+let durationInDays = 4;
+let activities = ['Flower Viewing', 'Picnic', 'Outdoor Concert'];
+
+//Display Initial theme
+console.log('Initial Theme Plan');
+console.log('Theme Name:', themeName);
+console.log('Duration (days):', durationInDays);
+console.log('Activites:', activities.join(', '));
+console.log('\n');
+
+//Update the theme name 
+themeName = 'summer retreat';
+
+//Add new activity 
+activities.push('Beach Volleyball');
+
+//Display updated theme plan
+console.log('Updated theme plan:');
+console.log('Theme Name:', themeName);
