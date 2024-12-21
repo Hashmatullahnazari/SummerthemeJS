868d40738d7dd8eb52bf51163133d2cc4c2a3426
Hashmatullah Nazari <hn0153@gmail.com>
2024-12-21 12:33:43 -0600

    adventureWeek.js

---
+++ adventureWeek.js
@@ -0,0 +1,22 @@
+let themeName = "Adventure Week";
+let durationInDays = 7;
+let activities = ["Hiking", "Camping", "Kayaking"];
+let dateComponents = ["December", 21, 2024];
+
+
+console.log("=== Initial Theme Details===");
+console.log("Theme Name" + themeName);
+console.log("Duration in Days:", durationInDays);
+console.log("Date: ", dateComponents.join(", "));
+console.log("Activities:", activities.join(", "))
+
+
+//Update the theme name
+
+themeName = "Relaxation Retreat";
+console.log("\n=== Updated Theme Name ===");
+console.log("New Theme Name;" + themeName);
+
+//Add a new activity
+
+activities.push("Yoga");
